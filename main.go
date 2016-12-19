2d90b79aade0cf141cdd88c4bd6c9935ce733c5b
mynz <rollingteapot@gmail.com>
2016-12-19 19:03:53 +0900

    hello

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"goji.io"
+	"goji.io/pat"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(r, "name")
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
+func main() {
+	fmt.Println("try-goji")
+
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+	err := http.ListenAndServe("localhost:3000", mux)
+	if err != nil {
+		panic(err)
+	}
+}
